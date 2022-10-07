0003f8b0a08a171bf12f1b13ef3a97d22a42841a
Artem-Baklanovv <artem.baklanovv@gmail.com>
2022-10-07 14:18:54 +0300

    rename folder

---
+++ HomeWork_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое целое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.WriteLine("Первое число " + numA + " больше чем второе " + numB);
+}
+else
+{
+    Console.WriteLine("Второе число " + numB + " больше чем первое " + numA);
+}
