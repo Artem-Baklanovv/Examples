b579f0f19b4b5bd8bd140ef0aa9d14cee72d7d1a
Artem-Baklanovv <artem.baklanovv@gmail.com>
2022-10-11 19:50:50 +0300

    Home work 15

---
+++ Lesson_2/HomeWork_15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int numberDayWeek = Convert.ToInt32(Console.ReadLine());
+
+if (numberDayWeek >= 1 && numberDayWeek <= 7)
+{
+    if (numberDayWeek == 6 | numberDayWeek == 7) Console.WriteLine("Сегодня выходной");
+    else Console.WriteLine("Сегодня рабочий день");
+}
+else Console.WriteLine("Не верное число");
