1b2554d235b1223502042830adfa382524762e71
Artem-Baklanovv <artem.baklanovv@gmail.com>
2022-10-11 19:46:58 +0300

    Home work 13

---
+++ Lesson_2/HomeWork_13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите любое число: ");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+string numStr = Convert.ToString(numberThree);
+int numDigit = numStr.Length;
+
+if (numDigit > 2)
+{
+     Console.WriteLine($"Третья цифра введённого числа: {numStr[2]}");
+}
+
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
+
+
