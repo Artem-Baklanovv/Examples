f556419368e91f41cf4f2f64d1eef1597c6a56aa
Artem-Baklanovv <artem.baklanovv@gmail.com>
2022-10-11 19:30:40 +0300

    Home work 10

---
+++ Lesson_2/HomeWork_10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+for (int ii = 1; ii < 3 && (numberTwo <= 100 | numberTwo > 999); ii++)
+{
+    Console.Write(ii + " Упс, ERROR 400! Введите трёхзначное число : ");
+    numberTwo = Convert.ToInt32(Console.ReadLine());
+}
+if (numberTwo <= 100 | numberTwo > 999)
+{
+    Console.WriteLine("Что-то пошло не так, программа прервана: ");
+}
+
+numberTwo = numberTwo / 10 % 10;
+Console.WriteLine($"Вторая цифра введённого числа: {numberTwo}");
