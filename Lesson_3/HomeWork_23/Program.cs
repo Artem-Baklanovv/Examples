82533a19299c56ec410b52a6bc386cc6d2ca7d43
Artem-Baklanovv <artem.baklanovv@gmail.com>
2022-10-12 22:35:24 +0300

    Home work 23

---
+++ Lesson_3/HomeWork_23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите любое число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{N} -> ");
+
+PrintSquares(N);
+
+static void PrintSquares(int N)
+{
+    for(int i = 1; i < N; i++)
+    {
+        Console.Write($"{i*i*i}, ");
+    }
+    Console.Write($"{N*N*N}");
+
+}
