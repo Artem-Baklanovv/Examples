8139d9e070a9b34fecd1d8d23755288d4b669985
Artem-Baklanovv <artem.baklanovv@gmail.com>
2022-10-21 17:58:24 +0300

    Home work 38 and correction 36

--- Lrsson_5/HomeWork_36/Program.cs
+++ Lrsson_5/HomeWork_36/Program.cs
@@ -6,25 +6,25 @@ Console.WriteLine("Массив: ");
 PrintArray(numbers);
 int sum = 0;
 
-for (int i = 0; i < numbers.Length; i+=2)
+for (int i = 0; i < numbers.Length; i += 2)
     sum = sum + numbers[i];
 
-    Console.WriteLine($"Всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+Console.WriteLine($"Всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
 
 void FillArrayRandomNumbers(int[] numbers)
 {
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = new Random().Next(1,10);
-        }
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 10);
+    }
 }
 void PrintArray(int[] numbers)
 {
     Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write(numbers[i] + " ");
-        }
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
     Console.Write("]");
     Console.WriteLine();
 }
